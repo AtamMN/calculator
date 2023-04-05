a8ca1fae6ddfc32fcf434d77fd95f1ce5ae1d1d7
AtamMN <mkhatamramadhan@gmail.com>
2023-04-05 11:04:24 +0700

    calculator first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./index.html"
+    ],
+    theme: {
+        colors: {
+            'main-t1': '#2B4173',
+            'second-t1': '#232C43',
+            'third-t1' : '#182034',
+            'btn-eq1' : '#D03F2F',
+            'btn-pr1' : '#EAE3DC',
+            'btn-delpr1' : '#637097',
+            'btn-sh-pr1' : '#B4A597'
+        },
+        fontFamily: {
+            'main-f' : 'Ubuntu'
+        },
+        extend: {},
+    },
+    plugins: [],
+  }
